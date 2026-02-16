2abd24a9233c634ae1ebf2deb04d082fcb7c9828
Bryce <bholland@uccs.edu>
2026-02-16 19:45:33 +0000

    Task1 complete

--- homework1/tests/test_task1.py
+++ homework1/tests/test_task1.py
@@ -1,7 +1,8 @@
 import pytest
 from homework1.src.task1 import helloWorld
 
-def testHW():
-    helloWorld()
-    #assert helloWorld() == "Hello, World!"
+def testHW(capsys):
+    helloWorld() # Run the original function
+    output = capsys.readouterr() # capsys.readouterr() captures .err and .out, which is the stdout
+    assert output.out == "Hello, World!\n" # verify that the output matches
 
